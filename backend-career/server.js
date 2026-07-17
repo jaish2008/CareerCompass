70d5d6b5d5093e50af9fc104fba17c013f7a17b1
kiranpreetkaur4573-ui <kiranpreetkaur4573@gmail.com>
2026-07-17 21:56:07 +0530

    Create server.js

---
+++ backend-career/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const admin = require('firebase-admin');
+require('dotenv').config();
+
+const serviceAccount = require('./serviceAccountKey.json');
+admin.initializeApp({
+  credential: admin.cert(serviceAccount)
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const internshipRoutes = require('./internshiproutes');
+app.use('/api/internships', internshipRoutes);
+
+const { startScheduledSync } = require('./adzunaintegration');
+startScheduledSync();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
